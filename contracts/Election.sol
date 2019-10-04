a014c24148f6b7e705b4da8e70cc86d33f4f7e54
unknown <gomez7@uwm.edu>
2019-10-03 22:12:23 -0500

    2. List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-	// Store canidate
-	// Read canidate
-	string public candidate;
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	// Store a candidate
+
+	// Fetch a candidate
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates count
+	uint public candidatesCount;
+
 	// Constructor
 	constructor() public {
-		candidate = "Candidate  1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
 }
